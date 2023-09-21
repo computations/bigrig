d9ebe2cb07493bb65579f2c43b65aed719e20dd5
computations <bwbettisworth@gmail.com>
2023-09-21 17:37:43 -0400

    Implements statistical tests, still unfinished

--- tests/src/dist.cpp
+++ tests/src/dist.cpp
@@ -1,8 +1,10 @@
 #include "model.hpp"
 #include <catch2/catch_get_random_seed.hpp>
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
 #include <dist.hpp>
 #include <iostream>
+#include <math.h>
 #include <random>
 
 TEST_CASE("dist operations", "[dist]") {
@@ -59,5 +61,71 @@ TEST_CASE("sample", "[sample]") {
   auto t = biogeosim::sample(init_dist, model, gen);
   CHECK(t.initial_state == init_dist);
   CHECK(t.initial_state != t.final_state);
-  CHECK(t.waiting_time < brlen);
+}
+
+TEST_CASE("stats", "[sample][stats]") {
+  const biogeosim::dist_t init_dist = 0b1010;
+  std::minstd_rand gen(Catch::getSeed());
+  SECTION("e = d = t = 1.0") {
+    constexpr size_t regions = 4;
+    constexpr double dis = 1.0;
+    constexpr double ext = 1.0;
+
+    constexpr double brlen = 1.0;
+
+    constexpr size_t iters = 1e6;
+    constexpr double mu = 1.0;
+    constexpr double sigma = mu;
+
+    biogeosim::substitution_model_t model(dis, ext, regions);
+
+    double mean = 0;
+    double std = 0;
+
+    for (size_t i = 0; i < iters; ++i) {
+      auto trans = biogeosim::generate_samples(brlen, model, gen);
+      double num = trans.size();
+      mean += num;
+      std += (num - mu) * (num - mu);
+    }
+
+    mean /= iters;
+    std /= iters;
+
+    CHECK_THAT(mean - mu, Catch::Matchers::WithinAbs(0.0, 0.01));
+    CHECK_THAT(std, Catch::Matchers::WithinAbs(sigma, 0.01));
+  }
+  SECTION("e = d = 4.0, t = 1.0") {
+    constexpr size_t regions = 4;
+    constexpr double dis = 4.0;
+    constexpr double ext = 4.0;
+
+    constexpr double brlen = 1.0;
+
+    constexpr size_t iters = 1e2;
+    constexpr double mu = 4.0;
+    constexpr double sigma = 1.0 / (mu * mu) / sqrt(iters);
+
+    biogeosim::substitution_model_t model(dis, ext, regions);
+
+    double sum = 0;
+    double sumsq = 0;
+
+    for (size_t i = 0; i < iters; ++i) {
+      double count = biogeosim::generate_samples(brlen, model, gen).size();
+      sum += count;
+      sumsq += count * count;
+    }
+
+    INFO("Sum is:" << sum);
+    INFO("sumsq is:" << sumsq);
+
+    double mean = sum / iters;
+    double std = (sumsq - sum * sum / iters) / (iters - 1);
+    double t = (mean - mu) / (std / sqrt(iters));
+
+    CHECK_THAT(mean - mu, Catch::Matchers::WithinAbs(0.0, 0.01));
+    CHECK_THAT(std, Catch::Matchers::WithinAbs(sigma, 0.01));
+    CHECK_THAT(t, Catch::Matchers::WithinAbs(0, 0.01));
+  }
 }
