b0763c2683dd239f65e1e399ca11eefee9b83387
computations <bwbettisworth@gmail.com>
2023-10-18 18:55:55 -0400

    Implement phylip output, change how the prefix is treated

--- src/main.cpp
+++ src/main.cpp
@@ -8,19 +8,29 @@
 #include <CLI/Formatter.hpp>
 #include <corax/corax.hpp>
 #include <filesystem>
+#include <fstream>
 #include <logger.hpp>
 #include <optional>
 #include <sstream>
 #include <string>
 
+constexpr auto PHYILP_EXT = ".phy";
+
 struct cli_options_t {
   std::filesystem::path tree_filename;
   std::filesystem::path prefix;
   bool                  debug_log;
-  uint64_t              root_distribution;
-  size_t                regions;
+  biogeosim::dist_t     root_distribution;
+  uint16_t              regions;
   double                dispersion_rate;
   double                extinction_rate;
+  bool                  redo;
+
+  std::filesystem::path phylip_filename() const {
+    auto tmp  = prefix;
+    tmp      += PHYILP_EXT;
+    return tmp;
+  }
 };
 
 std::string to_phylip(const biogeosim::tree_t              &tree,
@@ -79,23 +89,20 @@ validate_tree_filename(const std::filesystem::path &tree_filename) {
 
 [[nodiscard]] bool validate_prefix(const std::filesystem::path &prefix) {
   bool ok = true;
-  if (!std::filesystem::exists(prefix)) {
-    LOG_WARNING("The path '%s' does not exist", prefix.c_str());
+  if (!std::filesystem::exists(prefix.parent_path())) {
+    LOG_WARNING("The path '%s' does not exist", prefix.parent_path().c_str());
 
     try {
-      std::filesystem::create_directories(prefix);
+      std::filesystem::create_directories(prefix.parent_path());
     } catch (std::filesystem::filesystem_error err) {
       LOG_ERROR("%s", err.what());
       ok = false;
     }
-  } else if (!verify_is_writable(prefix)) {
+  } else if (!verify_is_writable(prefix.parent_path())) {
     LOG_ERROR("The prefix '%s' is not writable", prefix.c_str());
     ok = false;
   }
-  if (!std::filesystem::is_directory(prefix)) {
-    LOG_ERROR("The prefix '%s' is not at directory", prefix.c_str());
-    ok = false;
-  }
+
   return ok;
 }
 
@@ -106,7 +113,7 @@ validate_tree_filename(const std::filesystem::path &tree_filename) {
   ok &= validate_prefix(cli_options.prefix);
 
   if (cli_options.regions > 64) {
-    LOG_ERROR("Simulating with %lu regions is unsupported. Please choose a "
+    LOG_ERROR("Simulating with %u regions is unsupported. Please choose a "
               "number less than or equal to 64",
               cli_options.regions);
     ok = false;
@@ -131,6 +138,8 @@ void write_header(const cli_options_t &cli_options) {
   MESSAGE_INFO("Running simulation with the following parameters:");
   LOG_INFO("  Tree file: %s", cli_options.tree_filename.c_str());
   LOG_INFO("  Prefix: %s", cli_options.prefix.c_str());
+  LOG_INFO("  Root Distribution: %s",
+           cli_options.root_distribution.to_str().c_str());
 }
 
 bool normalize_paths(cli_options_t &cli_options) {
@@ -159,6 +168,17 @@ bool normalize_paths(cli_options_t &cli_options) {
   return ok;
 }
 
+[[nodiscard]] bool check_existing_results(const cli_options_t &cli_options) {
+  bool ok = true;
+
+  if (std::filesystem::exists(cli_options.phylip_filename())) {
+    LOG_WARNING("Results file %s exists already",
+                cli_options.phylip_filename().c_str());
+    ok = false;
+  }
+  return ok;
+}
+
 int main(int argc, char **argv) {
   logger::get_log_states().add_stream(
       stdout,
@@ -175,20 +195,24 @@ int main(int argc, char **argv) {
                  "A file containing a newick encoded tree which will be used "
                  "to perform the simulation")
       ->required();
-  app.add_option("--prefix", cli_options.prefix, "prefix for the output files")
-      ->required();
+  app.add_option("--prefix", cli_options.prefix, "prefix for the output files");
   app.add_option("--root-dist",
                  cli_options.root_distribution,
-                 "Distribution at the root at the start of the simulation");
-  app.add_option(
-      "--regions", cli_options.regions, "Number of regions to simulate");
+                 "Distribution at the root at the start of the simulation")
+      ->required();
   app.add_option("--d", cli_options.dispersion_rate, "Dispersion rate");
   app.add_option("--e", cli_options.extinction_rate, "Extinction rate");
+  app.add_flag("--redo", cli_options.redo, "Extinction rate")
+      ->default_val(false);
   app.add_flag("--debug-log",
                cli_options.debug_log,
                "Create a file in the prefix that contains the debug log");
 
   CLI11_PARSE(app);
+
+  if (cli_options.prefix.empty()) {
+    cli_options.prefix = cli_options.tree_filename;
+  }
 
   normalize_paths(cli_options);
   write_header(cli_options);
@@ -199,8 +223,17 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  if (!check_existing_results(cli_options)) {
+    if (!cli_options.redo) {
+      MESSAGE_ERROR("Refusing to run with existing results. Please specify the "
+                    "--redo option if you want to overwrite existig results");
+      return 1;
+    }
+  }
+
   if (cli_options.debug_log) {
-    std::filesystem::path debug_filename = cli_options.prefix / "debug.log";
+    std::filesystem::path debug_filename  = cli_options.prefix;
+    debug_filename                       += ".debug.log";
     LOG_INFO("Logging debug information to %s", debug_filename.c_str());
     logger::get_log_states().add_file_stream(
         debug_filename.c_str(),
@@ -214,11 +247,17 @@ int main(int argc, char **argv) {
   pcg_extras::seed_seq_from<std::random_device> seed_source;
   pcg64_fast                                    gen(seed_source);
 
-  biogeosim::substitution_model_t model(1.0, 1.0, 6);
+  biogeosim::substitution_model_t model(
+      cli_options.dispersion_rate,
+      cli_options.extinction_rate,
+      cli_options.root_distribution.regions());
 
-  tree.sample({0b11'0011, 6}, model, gen);
-  std::cout << tree.to_newick() << std::endl;
-  std::cout << to_phylip(tree, model) << std::endl;
+  tree.sample(cli_options.root_distribution, model, gen);
+
+  auto phylip_filename  = cli_options.prefix;
+  phylip_filename      += ".phy";
+  std::ofstream phylip_file(phylip_filename);
+  phylip_file << to_phylip(tree, model);
 
   return 0;
 }
